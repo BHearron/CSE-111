d0c9a2cfe1336cc45eae9938f848d3dfb18d6963
bh <bhearron@ucsc.edu>
2023-05-25 01:07:27 +0000

    Making robot server classes

--- robot_client/src/Main.cpp
+++ robot_client/src/Main.cpp
@@ -1,6 +1,4 @@
-/*
- * Socket setup help: https://www.geeksforgeeks.org/socket-programming-cc/#
- */
+/* Socket setup help: https://www.geeksforgeeks.org/socket-programming-cc/# */
 
 
 #include <iostream>
@@ -14,13 +12,40 @@
 #include <errno.h> 
 #include <unistd.h>
 
+#include <string.h>  // for testing purposes
+
+#include <chrono>
+#include <thread>
+
 #define PORT 8888
+
+
+/* Credit to Week7 LectureB slides */
+uint64_t timeSinceEpochMillisec() {
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+
+int send_message(int &sckt_fd) {
+  while(1) {
+    const char* msg = "tick";
+    int rtn = write(sckt_fd, msg, strlen(msg));
+    std::cerr << "rtn: " << rtn << " |  sent tick. Sleeping for 500ms" << std::endl;
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(500));
+    // sleep(1);
+  }
+  std::cerr << "how did i get here?" << std::endl;
+  return EXIT_SUCCESS;
+}
+
 
 int setup_socket(int &sckt_fd, struct sockaddr_in &serv_addr) {
   // create socket
-  sckt_fd = socket(AF_INET, SOCK_STREAM, 0)
+  sckt_fd = socket(AF_INET, SOCK_STREAM, 0);
   if (sckt_fd < 0) {
-    cout:cerr << "\n Socket creation error \n" << std::endl;
+    std::cerr << "\n Socket creation error \n" << std::endl;
     exit(EXIT_FAILURE); 
   }
 
@@ -28,30 +53,28 @@ int setup_socket(int &sckt_fd, struct sockaddr_in &serv_addr) {
   serv_addr.sin_family = AF_INET;
   serv_addr.sin_port = htons(PORT);
 
+  // Convert ip address
   if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
-    std:cerr << "\nInvalid address/ Address not supported \n" << std::endl;
+    std::cerr << "\nInvalid address/ Address not supported \n" << std::endl;
     exit(EXIT_FAILURE); 
   }
 
   // connect to server
   if ((connect(sckt_fd, (struct sockaddr*)&serv_addr, sizeof(serv_addr))) < 0) {
-    std:cerr << "\nConnection Failed \n" << std::endl;
+    std::cerr << "\nConnection Failed \n" << std::endl;
     exit(EXIT_FAILURE); 
   }
 
-  handle_messages(sckt_fd);
-
   return EXIT_SUCCESS;
 }
 
-int handle_messages(int &sckt_fd) {
-  return EXIT_SUCCESS;
-}
 
 int main(int argc, char * argv[]) {
   int sckt_fd;
   struct sockaddr_in serv_addr;
+
   setup_socket(sckt_fd, serv_addr);
+  send_message(sckt_fd);
 
 	return EXIT_SUCCESS;
 }
--- robot_server/src/Main.cpp
+++ robot_server/src/Main.cpp
@@ -10,10 +10,80 @@
 #include <errno.h> 
 #include <unistd.h>
 
+#include <string.h>  // used for testing
+
+#include <chrono>
+#include <thread>
+
+#include "Message.h"
+
 #define PORT 8888
 
-// global state variable here
-int state = 0;
+
+/* Credit to Week7 LectureB slides */
+class Tickable {
+
+  uint64_t last_tick_time = 0;
+  std::string last_tick_time_string = "";
+
+  public:
+
+    virtual void tick(const small_world::SM_Event & event) {
+      // convert string to uint64_t
+      char* end;
+      last_tick_time = strtoull(event.event_time().c_str(), &end, 10);
+      std::cout << last_tick_time << std::endl;
+    }
+};
+
+class StateMachine {
+
+};
+
+class RobotState {
+  uint64_t init_time;
+  uint64_t curr_time;
+
+  std::map<std::string, std::shared_ptr<RobotState>> next_states;
+  std::shared_ptr<StateMachine> owner;
+
+  uint64_t get_elapsed() {
+    return curr_time - init_time;
+  }
+
+  void set_next_state(const std::string & state_name, std::shared_ptr<RobotState> state) {
+    next_states[state_name] = state;
+  }
+};
+
+
+
+
+/* Credit to Week7 LectureB slides */
+uint64_t timeSinceEpochMillisec() {
+  using namespace std::chrono;
+  return duration_cast<milliseconds>(system_clock::now().time_since_epoch()).count();
+}
+
+
+int handle_message(int &sckt) {
+  int client_msg_len = 0;
+  do {
+    // get and echo request from client
+    char buf[1025] = {0};
+    client_msg_len = read(sckt, buf, 1024);
+    std::cerr << buf << std::endl;
+
+    // carry out client request
+
+    // send client success
+    const char* resp = "Success";
+    write(sckt, resp, strlen(resp));
+  }
+  while (client_msg_len > 0);
+
+  return EXIT_SUCCESS;
+}
 
 
 int socket_setup(int &sckt, struct sockaddr_in &address) {
@@ -25,7 +95,8 @@ int socket_setup(int &sckt, struct sockaddr_in &address) {
   }
 
   // let the socket be re-used
-  if( setsockopt(master_socket, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )  
+  int opt = true;
+  if( setsockopt(sckt, SOL_SOCKET, SO_REUSEADDR, (char *)&opt, sizeof(opt)) < 0 )  
   {  
     std::cerr << "setsockopt" << std::endl;  
     exit(EXIT_FAILURE);  
@@ -47,8 +118,7 @@ int socket_setup(int &sckt, struct sockaddr_in &address) {
 }
 
 
-int listen_routine(int &sckt) {
-
+int listen_routine(int &sckt, struct sockaddr_in &address) {
   // listen to, at max, 3 connections
   if (listen(sckt, 3) < 0)  
   {  
@@ -56,27 +126,23 @@ int listen_routine(int &sckt) {
     exit(EXIT_FAILURE);  
   }
 
-  addrlen = sizeof(address);
+  // listen for new requests to connect
+  int addrlen = sizeof(address);
   while (true) {
-    // listen infinitely for a new message to accept
-    int accepted_sckt = accept(server_fd, (struct sockaddr*)&address, (socklen_t*)&addrlen)
+    // accept incoming connection
+    int accepted_sckt = accept(sckt, (struct sockaddr*)&address, (socklen_t*)&addrlen);
     if (accepted_sckt < 0) {
         perror("accept");
         exit(EXIT_FAILURE);
     }
 
-    // read message from accepted 
-    // socket then clean up
+    // read and handle message from client
     handle_message(accepted_sckt);
+
+    // clean up
     close(accepted_sckt);
   }
 
-  return EXIT_SUCCESS;
-}
-
-
-int handle_message(int &sckt) {
-  // read messages here
   return EXIT_SUCCESS;
 }
 
@@ -95,12 +161,13 @@ int main(int argc, char * argv[]) {
   std::string robot_began_waiting = "The robot began waiting";
   std::string robot_begin_moving = "The robot begain moving";
 
-
+  
+  // the connection routine below
   int sckt;
   struct sockaddr_in address;
-  socket_setup(sckt, address);
 
-  listen_routine(sckt);
+  socket_setup(sckt, address);
+  listen_routine(sckt, address);
 
 	return EXIT_SUCCESS;
 }
